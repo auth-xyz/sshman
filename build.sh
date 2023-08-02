7422c395d23f7f6b935ad2217c43526bcc12c966
auth-xyz <authenticover@gmail.com>
2023-08-02 01:23:13 -0500

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,43 +1,38 @@
-#! /usr/bin/bash
+#!/usr/bin/env bash
 
 version="mount-temple"
 
-# Windows
-wine pyinstaller --onefile sshman.py
-rm -rf build
-# shellcheck disable=SC2035
-rm *.spec
+build_windows() {
+    wine pyinstaller --onefile sshman.py
+    rm -rf build *.spec
+    cd dist/
+    cat ../Bin/win_install.bat >> ./install.bat
+    chmod +x sshman
+    cd ..
+    tar cfz "win64-$version.tar.gz" dist/*
+    echo "[ sshman : Windows building process was finished ]"
+}
 
-# shellcheck disable=SC2164
-cd dist/
-cat ../Bin/win_install.bat >> ./install.bat
-chmod +x sshman
-# shellcheck disable=SC2103
-cd ..
+build_linux() {
+    pyinstaller --onefile sshman.py
+    rm -rf build *.spec
+    cd dist/
+    cat ../Bin/linux_install.sh >> ./install.sh
+    chmod +x install.sh
+    chmod +x sshman
+    cd ..
+    tar cfz "linux-$version.tar.gz" dist/*
+    echo "[ sshman : Linux building process was finished ]"
+}
 
-tar cfz win64-$version.tar.gz dist/*
-clear
-# shellcheck disable=SC2035
-mv *.tar.gz ~/Downloads/
-echo "[ sshman : Windows building process was finished ]"
+clean_up() {
+    rm -rf dist/
+}
 
-# Clean up for Linux build
-rm -rf dist/
+build_windows
+clean_up
+build_linux
+clean_up
 
-# Linux
-pyinstaller --onefile sshman.py
-rm -rf build
-rm *.spec
-
-# shellcheck disable=SC2164
-cd dist/
-cat ../Bin/linux_install.sh >> ./install.sh
-chmod +x install.sh
-chmod +x sshman
-cd ..
-
-tar cfz linux-$version.tar.gz dist/*
-clear
-# shellcheck disable=SC2035
-mv *.tar.gz ~/Downloads/
-echo "[ sshman : Linux building process was finished ]"
+mv "win64-$version.tar.gz" "linux-$version.tar.gz" ~/Downloads/
+echo "[ sshman : All building processes were finished ]"
